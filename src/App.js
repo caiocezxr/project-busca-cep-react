b19d80ce8def31af70cfce97f534fd50050f1221
Caio Cezar <caiocdsm@hotmail.com>
2023-07-04 15:45:05 -0300

    projeto busca cep com estilizacao

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi'
+import './style.css';
+import api from './services/api'
+import img from './img/11b51bd2-aedc-40b0-ad8b-48386b457eef.jpeg'
+// import { IoMdMap } from 'react-icons/io';
+// import img2 from './img2/img631cd45cd594e3.82000930.jpg'
+
 
 function App() {
+  
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({}); //aqui fica o objeto com os itens
+  
+  async function handleSearch (){
+    if(input === ''){
+      alert('Preencha o campo!')
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput('')
+    }catch{
+      alert("Erro ao buscar CEP");
+      setInput('')
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+          <h1 className="title">BUSCA RATO</h1>
+        <div className="containerInput">
+          <input type="text"
+          placeholder="Digite seu CEP"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+          />
+          <button className="btnSearch" onClick={handleSearch}>
+            <FiSearch size={25} color='#FFF'/>
+          </button>
+      </div>
+
+    {Object.keys(cep).length > 0 && ( //essa e a forma de verificar se tem alguma propriedade dentro do objeto. se o tamanho é maior que 0 tem alguma coisa.
+        <main className='main'>
+        
+        <h2>CEP: {cep.cep} </h2>
+        <span> {cep.logradouro} </span>
+        <span>{cep.complemento}</span>
+        <span>{cep.bairro}</span>
+        <span>{cep.localidade} - {cep.uf} </span>
+        <img className='rato' src={img} alt=''/>
+      </main>
+)}
+
     </div>
   );
 }
---
+++ src/services/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+//https://viacep.com.br/ws/60811110/json/
+const api = axios.create ({
+    baseURL: 'https://viacep.com.br/ws/'
+});
+
+export default api
